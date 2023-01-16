1a36f9c076e6b3cae5638c9eea2c0a911bd32343
cherbourg@fub.fr <cherbourg@fub.fr>
2023-01-16 12:44:10 +0100

    Add photo ag

--- pages/Actus.vue
+++ pages/Actus.vue
@@ -5,7 +5,7 @@
   <div class="mb-4 md:mb-0 w-full max-w-screen-md mx-auto relative" style="height: 24em;">
   <div class="absolute left-0 bottom-0 w-full h-full z-10"
     style="background-image: linear-gradient(180deg,transparent,rgba(0,0,0,.7));"></div>
-  <img src="@/assets/cyclistesbrillez.jpeg" class="absolute left-0 top-0 w-full h-full z-0 object-cover" />
+  <img src="@/assets/ag26.jpg" class="absolute left-0 top-0 w-full h-full z-0 object-cover" />
   <div class="p-4 absolute bottom-0 left-0 z-20">
     <a href="#" class="px-4 py-1 bg-black text-gray-200 inline-flex items-center justify-center mb-2">26/01/2023</a>
     <h2 class="text-4xl font-semibold text-gray-100 leading-tight">📢 Assemblée générale</h2>
