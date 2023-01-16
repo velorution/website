f8339fff1db253209a175c69e6416f90ccf34fb7
cherbourg@fub.fr <cherbourg@fub.fr>
2023-01-16 12:00:56 +0100

    correct fautes

--- pages/Actus.vue
+++ pages/Actus.vue
@@ -8,7 +8,7 @@
   <img src="@/assets/cyclistesbrillez.jpeg" class="absolute left-0 top-0 w-full h-full z-0 object-cover" />
   <div class="p-4 absolute bottom-0 left-0 z-20">
     <a href="#" class="px-4 py-1 bg-black text-gray-200 inline-flex items-center justify-center mb-2">26/01/2023</a>
-    <h2 class="text-4xl font-semibold text-gray-100 leading-tight">Assemblé Générale</h2>
+    <h2 class="text-4xl font-semibold text-gray-100 leading-tight">Assemblée Générale</h2>
     <div class="flex mt-3">
     </div>
   </div>
@@ -41,7 +41,6 @@ Tous les membres de l'association sont invités à participer à ces Assemblées
 <br>
 </div>
 
-</div>
 
 <div class="mb-4 md:mb-0 w-full max-w-screen-md mx-auto relative" style="height: 24em;">
   <div class="absolute left-0 bottom-0 w-full h-full z-10"
